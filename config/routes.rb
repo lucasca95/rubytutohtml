d023290e2744f3eb705017ad4cbe69194d8a01cf
lucasca95 <lucasca95@yahoo.com.ar>
2019-08-20 21:17:47 -0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   get 'home/notas'
 
   root 'home#index'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
